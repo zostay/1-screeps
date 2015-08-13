a9781654a44922d027114b59d63bde97c2245bc8
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-12 22:24:33 -0500

    tweaking guard patrolling

--- guard.js
+++ guard.js
@@ -11,11 +11,12 @@ module.exports = function (creep) {
         }
     }
     else {
-        var x = Math.random() * 50;
-        var y = Math.random() * 50;
+        var x = Math.random() * 49 + 1;
+        var y = Math.random() * 49 + 1;
         var spot = creep.room.getPositionAt(x, y);
         var stuff = spot.look();
         for (var i in stuff) {
+            console.log('[' + spot.x + ',' + spot.y + ']: ' + stuff[i].type);
             if (stuff[i].type === 'terrain' && stuff[i].terrain === 'plain') {
                 creep.memory.patrolTo = spot;
             }
