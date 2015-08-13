3649c3f18390d20679b9cf8cbcac2427562f8a70
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-13 09:52:11 -0500

    better spawning for builders and harvesters

--- spawner.js
+++ spawner.js
@@ -1,7 +1,30 @@
+function findNameByRole(spawn, role) {
+    var index = 0;
+    var creeps = Game.creeps;
+    while (creeps[ spawn.room.name + '.' + role + '.' + index ]) index++;
+    return spawn.room.name + '.' + role + '.' + index;
+}
+
+function spawnByRole(spawn, creeps, role, parts) {
+    var name = findNameByRole(spawn, role);
+    spawn.createCreep(parts, name, { role: role });
+}
+
 function checkCreepSupply(spawn, all, creeps, role, count, parts) {
     if (!creeps[role] || creeps[role].length < count) {
-        var count = creeps[role] ? creeps[role].length : 0;
-        spawn.createCreep(parts, role + count, { role: role });
+        spawnByRole(creeps, role, parts);
+    }
+}
+
+var LIFETIME = 1500;
+function spawnCreepEvery(spawn, creeps, role, ticks, parts) {
+    if (Game.time % ticks == 0 || spawn.memory[role]) {
+        if (spawnByRole(spawn, creeps, role, parts) == OK) {
+            spawn.memory[role] = false;
+        }
+        else {
+            spawn.memory[role] = true;
+        }
     }
 }
 
@@ -14,11 +37,10 @@ module.exports = function (spawn) {
         roleCreeps[ thisCreep.memory.role ].push(thisCreep);
     }
 
-    var sources = spawn.room.find(FIND_SOURCES);
-    checkCreepSupply(spawn, allCreeps, roleCreeps, 'harvester', sources.length, [WORK,CARRY,MOVE,MOVE]);
-    checkCreepSupply(spawn, allCreeps, roleCreeps, 'builder',   2, [WORK,CARRY,MOVE,MOVE]);
-
     var targets = spawn.room.find(FIND_HOSTILE_CREEPS);
     var min = targets.length < 1 ? 1 : targets.length;
     checkCreepSupply(spawn, allCreeps, roleCreeps, 'guard', min, [TOUGH,ATTACK,MOVE,MOVE]);
+
+    spawnCreepEvery(spawn, roleCreeps, 'builder',   LIFETIME / 2, [WORK,CARRY,MOVE,MOVE]);
+    spawnCreepEvery(spawn, roleCreeps, 'harvester', LIFETIME / 2, [WORK,CARRY,MOVE,MOVE]);
 }
