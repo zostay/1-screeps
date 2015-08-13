5b37d8a9e00f033e562fd0056c6b5a534c075328
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-13 13:40:21 -0500

    add bigger builders

--- spawner.js
+++ spawner.js
@@ -46,6 +46,11 @@ function spawnFromQueue(spawn, creeps) {
     }
 }
 
+var HARVESTER_BODY   = [ WORK, CARRY, MOVE, MOVE ];
+var BUILDER_BODY     = [ WORK, CARRY, MOVE, MOVE ];
+var BIG_BUILDER_BODY = [ WORK, WORK, CARRY, CARRY, MOVE, MOVE ];
+var GUARD_BODY       = [ TOUGH, ATTACK, MOVE, MOVE ];
+
 module.exports = function (spawn) {
     var allCreeps = spawn.room.find(FIND_MY_CREEPS);
     var roleCreeps = {};
@@ -55,14 +60,29 @@ module.exports = function (spawn) {
         roleCreeps[ thisCreep.memory.role ].push(thisCreep);
     }
 
-    spawnCreepEvery(spawn, roleCreeps, 'harvester', LIFETIME / 2, [WORK,CARRY,MOVE,MOVE]);
-    checkCreepSupply(spawn, allCreeps, roleCreeps, 'harvester', 1, [WORK,CARRY,MOVE,MOVE]);
+    var totalEnergy = spawn.energy;
+    var storages = creep.room.find(FIND_MY_STRUCTURES, {
+        filter: function(s) {
+            return s.structureType == STRUCTURE_EXTENSION;
+        }
+    });
+    for (var i in storages) {
+        totalEnergy += storages[i].energy;
+    }
+
+    spawnCreepEvery(spawn, roleCreeps, 'harvester', LIFETIME / 2, HARVESTER_BODY);
+    checkCreepSupply(spawn, allCreeps, roleCreeps, 'harvester', 1, HARVESTER_BODY)
 
     var targets = spawn.room.find(FIND_HOSTILE_CREEPS);
     var min = targets.length < 1 ? 1 : targets.length;
-    checkCreepSupply(spawn, allCreeps, roleCreeps, 'guard', min, [TOUGH,ATTACK,MOVE,MOVE]);
+    checkCreepSupply(spawn, allCreeps, roleCreeps, 'guard', min, GUARD_BODY);
 
-    spawnCreepEvery(spawn, roleCreeps, 'builder',   LIFETIME / 2, [WORK,CARRY,MOVE,MOVE]);
+    if (totalEnergy > 400) {
+        spawnCreepEvery(spawn, roleCreeps, 'builder', LIFETIME / 2, BIG_BUILDER_BODY);
+    }
+    else {
+        spawnCreepEvery(spawn, roleCreeps, 'builder', LIFETIME / 2, BUILDER_BODY);
+    }
 
     spawnFromQueue(spawn, roleCreeps);
 }
