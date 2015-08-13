e23e1d81156dd67d5cb6c2c9aea74be929f59570
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-13 13:42:37 -0500

    announce spawns

--- spawner.js
+++ spawner.js
@@ -7,6 +7,7 @@ function findNameByRole(spawn, role) {
 
 function spawnByRole(spawn, creeps, role, parts) {
     var name = findNameByRole(spawn, role);
+    console.log("Spawning " + role + ": " + name);
     return spawn.createCreep(parts, name, { role: role }) == name;
 }
 
