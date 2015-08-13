42c1a1b62d6bc79efb07bed4db389d84d723975e
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-12 22:13:38 -0500

    fix bug in creep patrol

--- guard.js
+++ guard.js
@@ -4,22 +4,20 @@ module.exports = function (creep) {
 	    creep.moveTo(targets[0]);
 	    creep.attack(targets[0]);
     }
-    else if (creep.patrolTo) {
-        console.log(creep.name + ' is patrolling');
-        creep.moveTo(creep.patrolTo);
-        if (creep.patrolTo.x == creep.x && creep.patrolTo.y == creep.y) {
+    else if (creep.memory.patrolTo) {
+        creep.moveTo(creep.memory.patrolTo);
+        if (creep.memory.patrolTo.x == creep.x && creep.patrolTo.y == creep.y) {
             creep.patrolTo = null;
         }
     }
     else {
-        console.log('finding patrol spot');
         var x = Math.random() * 50;
         var y = Math.random() * 50;
         var spot = creep.room.getPositionAt(x, y);
         var stuff = spot.look();
         for (var i in stuff) {
             if (stuff[i].type === 'terrain' && stuff[i].terrain === 'plain') {
-                creep.patrolTo = spot;
+                creep.memory.patrolTo = spot;
             }
         }
     }
