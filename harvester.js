c9e27b147894be39afed614be5d552c58b0843d5
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-13 10:54:59 -0500

    transfer energy to extensions too

--- harvester.js
+++ harvester.js
@@ -1,12 +1,22 @@
 module.exports = function (creep) {
-
 	if(creep.carry.energy < creep.carryCapacity) {
 		var sources = creep.room.find(FIND_SOURCES);
 		creep.moveTo(sources[0]);
 		creep.harvest(sources[0]);
 	}
 	else {
-		creep.moveTo(Game.spawns.Home);
-		creep.transferEnergy(Game.spawns.Home)
+        if (Game.spawns.Home.energy == Game.spawns.Home.energyCapacity) {
+            var storages = creep.room.find(FIND_MY_STRUCTURES, function(s) {
+                return s.structureType == STRUCTURE_EXTENSION
+                    && s.energy < s.energyCapacity;
+            });
+
+            creep.moveTo(storages[0]);
+            creep.transferEnergy(storages[0]);
+        }
+        else {
+    		creep.moveTo(Game.spawns.Home);
+    		creep.transferEnergy(Game.spawns.Home)
+        }
 	}
 }
