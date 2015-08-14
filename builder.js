7cc04fa2b4a3fd3c1a9bd2632c4fd9a522d62ee8
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-13 19:56:25 -0500

    make builders much smarter

--- builder.js
+++ builder.js
@@ -1,37 +1,58 @@
 module.exports = function (creep) {
-	if (creep.carry.energy == 0) {
-        if (Game.spawns.Home.energy > creep.carryCapacity / 2) {
-    		creep.moveTo(Game.spawns.Home);
-    		Game.spawns.Home.transferEnergy(creep);
+    creep.memory.state = creep.memory.state || 'build';
+    if (creep.memory.state == 'build') {
+        if (creep.carry.energy == 0) {
+            creep.say('Gather');
+            creep.memory.state = 'gather';
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES, {
-                filter: function(s) {
-                    return s.energy > 100;
-                }
-            });
+    		var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if (creep.room.controller.level < 2) {
+                creep.moveTo(creep.room.controller);
+                creep.upgradeController(creep.room.controller);
+            }
+    		else if(targets.length) {
+    			creep.moveTo(targets[0]);
+    			creep.build(targets[0]);
+    		}
 
-            if (sources.length) {
-                creep.moveTo(sources[0]);
-                creep.harvest(sources[0]);
+    		// might as well do something
+            else {
+                creep.moveTo(creep.room.controller);
+                creep.upgradeController(creep.room.controller);
             }
         }
-	}
-	else {
-		var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-        if (creep.room.controller.level < 2) {
-            creep.moveTo(creep.room.controller);
-            creep.upgradeController(creep.room.controller);
-        }
-		else if(targets.length) {
-			creep.moveTo(targets[0]);
-			creep.build(targets[0]);
-		}
+    }
+    else {
+        if (creep.memory.gatherFrom) {
+            creep.moveTo(creep.memory.gatherFrom);
+            if (creep.memory.gatherFrom.transferEnergy) {
+                creep.memory.gatherFrom.transferEnergy(creep);
+            }
+            else {
+                creep.harvest(creep.memory.gatherFrom);
+            }
 
-		// might as well do something
-        else {
-            creep.moveTo(creep.room.controller);
-            creep.upgradeController(creep.room.controller);
+            if (creep.carry.energy == creep.carryCapacity) {
+                creep.say('Build');
+                creep.memory.state = 'build';
+            }
         }
-	}
+        else {
+            if (Game.spawns.Home.energy > creep.carryCapacity / 2) {
+                creep.memory.gatherFrom = Game.spawns.Home;
+            }
+            else {
+                var sources = creep.room.find(FIND_SOURCES, {
+                    filter: function(s) {
+                        return s.energy > 100;
+                    }
+                });
+
+                if (sources.length) {
+                    creep.memory.gatherFrom = sources[0];
+                }
+            }
+        }
+    }
 }
