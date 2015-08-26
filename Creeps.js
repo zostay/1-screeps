c19691cec22fec7d1d3b587b4d7e2ce13c40d2c5
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-25 23:04:33 -0500

    do not announce state change unless the state changes

--- Creeps.js
+++ Creeps.js
@@ -44,8 +44,10 @@ Creeps.prototype.behave = function () {
 }
 
 Creeps.prototype.changeState = function (creep, state) {
-    creep.memory.state = state;
-    creep.say(state.substr(0, 1).toUpperCase() + state.substr(1));
+    if (creep.memory.state != state) {
+        creep.memory.state = state;
+        creep.say(state.substr(0, 1).toUpperCase() + state.substr(1));
+    }
 }
 
 Creeps.prototype.states = {};
