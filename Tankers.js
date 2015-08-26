976ce6d620a2802ff1c7117d762a7561c27a1d76
Sterling Hanenkamp <sterling@hanenkamp.com>
2015-08-26 07:29:13 -0500

    fix spelling of targetObj

--- Tankers.js
+++ Tankers.js
@@ -140,7 +140,7 @@ Tankers.prototype.states.deliver = function (creep) {
                 creep.transferEnergy(targetObj);
                 if (util.crowDistance(creep.pos, targetObj.pos) == 1)
                     creep.memory.target = null;
-                if (targetObj.carry && targetObj.carry.energy == target.carryCapacity)
+                if (targetObj.carry && targetObj.carry.energy == targetObj.carryCapacity)
                     creep.memory.target = null;
                 if (targetObj.energy == targetObj.energyCapacity)
                     creep.memory.target = null;
